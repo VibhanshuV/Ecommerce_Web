35a72c40e78ddad2f1edac4759f35847132afb64
Vibhanshu Vaibhav <vibhanshu@Vibhanshus-MacBook-Air.local>
2025-01-08 23:04:52 +0530

    Added pages & routes

---
+++ frontend/src/pages/admin-view/features.jsx
@@ -0,0 +1,9 @@
+function AdminFeatures() {
+    return (
+      <div>
+        <h1>Admin Features</h1>
+        <p>Admin can view all products, add new products, and delete products</p>
+      </div>
+    );
+  }       
+  export default AdminFeatures;
